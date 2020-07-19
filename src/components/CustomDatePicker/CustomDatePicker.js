7cea5a45ea1c48d410cbcc65488c1d9d3ede8674
Ganesh Karthik Natarajan <karthikprince2k1@gmail.com>
2020-07-18 23:45:38 -0400

    Modified Date Picker

--- src/components/CustomDatePicker/CustomDatePicker.js
+++ src/components/CustomDatePicker/CustomDatePicker.js
@@ -1,17 +1,65 @@
-import React, { useState } from "react";
+import React, { forwardRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const CustomDatePicker = React.forwardRef(
-  ({ label, name, options, register }, ref) => {
-    const [date, setDate] = useState(new Date());
-    return (
-      <>
-        <label>{label}</label>
-        <DatePicker selected={date} onChange={setDate} name={name} ref={ref} />
-      </>
-    );
-  }
-);
+const CustomDatePicker = forwardRef((props, ref) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const CustomDateInput = forwardRef((props, ref) => (
+    <input
+      onClick={props.onClick}
+      value={props.value}
+      onChange={props.onClick}
+      ref={props.register}
+      name={props.internalName}
+    />
+  ));
+  return (
+    <>
+      <label>{props.label}</label>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        customInput={
+          <CustomDateInput
+            internalName={props.name}
+            register={props.register}
+          />
+        }
+      />
+    </>
+  );
+});
+
+// function CustomDatePicker(props) {
+//   const [startDate, setStartDate] = useState(new Date());
+//   const CustomDateInput = (props) => {
+//     const required = props.required;
+//     console.log(props.internalName);
+//     return (
+//       <input
+//         value={props.value}
+//         name={props.internalName}
+//         ref={props.register({ required })}
+//       />
+//     );
+//   };
+//   console.log(props);
+//   return (
+//     <>
+//       <label>{props.label}</label>
+//       <DatePicker
+//         selected={startDate}
+//         onChange={(date) => setStartDate(date)}
+//         customInput={
+//           <CustomDateInput
+//             internalName={props.name}
+//             register={props.register}
+//             required={props.required}
+//           />
+//         }
+//       />
+//     </>
+//   );
+//}
 
 export default CustomDatePicker;
