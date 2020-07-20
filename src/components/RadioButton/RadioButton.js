f86d1e58d2ebc3bd030ee523aa80f5b912c30ca1
Ganesh Karthik Natarajan <karthikprince2k1@gmail.com>
2020-07-20 10:24:34 -0400

    Prefil for radio buttons

--- src/components/RadioButton/RadioButton.js
+++ src/components/RadioButton/RadioButton.js
@@ -1,14 +1,27 @@
-import React from "react";
-const RadioButton = ({ name, value, register, required }) => (
-  <>
-    <input
-      name={name}
-      ref={register({ required })}
-      value={value}
-      type="radio"
-    />
-    <label htmlFor={value}>{value}</label>
-  </>
-);
+import React, { useState } from "react";
+const RadioButton = ({
+  name,
+  value,
+  register,
+  required,
+  defaultValue,
+  label,
+}) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const handleOptionChange = (event) => setSelectedOption(event.target.value);
+  return (
+    <>
+      <input
+        name={name}
+        ref={register({ required })}
+        value={value}
+        type="radio"
+        checked={selectedOption === value || defaultValue === value}
+        onChange={handleOptionChange}
+      />
+      <label htmlFor={value}>{label}</label>
+    </>
+  );
+};
 
 export default RadioButton;
